6903800db3a7aebacd11e3e7e13ac213840d2cb9
Bucky Schwarz <d.w.schwarz@gmail.com>
2018-06-26 10:33:49 -0400

    have basic structure for website its ugly though

--- src/view/Container.jsx
+++ src/view/Container.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 let styles = theme => ({
   root: {
@@ -23,10 +28,36 @@ let Container = props => {
     <div className={classes.root}>
       <Grid container spacing={24}>
         <Grid item xs>
-          <Button>Add Task</Button>
+          <Paper className={classes.root} elevation={1}>
+            <Button>Add Task</Button>
+          </Paper>
         </Grid>
         <Grid item xs={8}>
-          <p>buttons</p>
+          <Grid container>
+            <Grid item xs>
+              <Typography variant="display2" gutterBottom>
+                Todo
+              </Typography>
+              <List>
+                <ListItem>
+                  <p>Item 1</p>
+                </ListItem>
+                <ListItem>
+                  <p>Item 2</p>
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid item xs>
+              <Typography variant="display2" gutterBottom>
+                In Progress
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <Typography variant="display2" gutterBottom>
+                Done
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
--- src/view/Main.jsx
+++ src/view/Main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from './Container';
 
-ReactDOM.render(<Container />, document.getElementById('app'));
+ReactDOM.render((
+  <React.Fragment>
+    <CssBaseline />
+    <Container />
+  </React.Fragment>),
+  document.getElementById('app')
+);
